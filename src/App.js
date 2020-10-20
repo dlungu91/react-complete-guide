3d7342502c74208333df6ad193dfdefe77e8a22e
Fred Lungu <flungu@SmartCare.org.zm>
2020-10-20 09:55:24 +0200

    Looking at react states

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
+/*
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
+      <p>From local machine :)</p>
+      <button>Switch name</button>
+      <Person name="Sharon" age="32" />
+      <Person name="Fred" age="29"> Hobbies: racing</Person>
     </div>
   );
+
+  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello React!'));
+}
+*/
+
+class App extends Component {
+  
+  state = {
+    persons: [
+      {name: 'Sharon', age: 32},
+      {name: 'Fred', age: 29}
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+
+    /*
+    Don't do this. Do not change the state directly
+
+    this.state.persons[0].name = 'Sharon Lungu';
+    */
+
+    this.setState(
+      {
+        persons: [
+          {
+            name: 'Sharon Lungu', age: 32
+          },
+          {
+            name: 'Fred Lungu', age: 29
+          }
+        ]
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello React!</h1>
+      <p>From local machine :)</p>
+      <button onClick={this.switchNameHandler}>Switch name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Hobbies: racing</Person>
+    </div>
+    )
+  }
 }
 
 export default App;
